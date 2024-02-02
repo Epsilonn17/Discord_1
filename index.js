3937690751a0585eb14335cba0ffc1403a94a5b3
Epsilonn17 <158550596+Epsilonn17@users.noreply.github.com>
2024-02-02 14:42:39 +0800

    Update index.js

--- index.js
+++ index.js
@@ -23,19 +23,19 @@ client.on('ready', async () => {
   console.log(`${client.user.tag} - rich presence started!`);
 
   const r = new Discord.RichPresence()
-    .setApplicationId('')
+    .setApplicationId('1167760523090219088')
     .setType('STREAMING')
-    .setURL('https://www.youtube.com/watch?v=dQw4w9WgXcQ') //Must be a youtube video link 
-    .setState('Recording')
-    .setName('')
-    .setDetails(`Valorant [${formatTime()}]`)
+    .setURL('https://www.youtube.com/watch?v=MtlA34oIUdU') //Must be a youtube video link 
+    .setState('Minecraft)
+    .setName('Epsilon')
+    .setDetails(`.gg/eporium`)
     .setStartTimestamp(Date.now())
- .setAssetsLargeImage('') //You can put links in tenor or discord and etc.
-    .setAssetsLargeText('Boring') //Text when you hover the Large image
-    .setAssetsSmallImage('') //You can put links in tenor or discord and etc.
-    .setAssetsSmallText('Twitch') //Text when you hover the Small image
-    .addButton('Watch', 'https://www.youtube.com/watch?v=dQw4w9WgXcQ')
-    .addButton('Donate', 'https://www.youtube.com/watch?v=dQw4w9WgXcQ');
+ .setAssetsLargeImage('https://media.discordapp.net/attachments/1193573791155097630/1202866001079963688/f8f107c8aeca73a593f2295cfaa029aa.png?ex=65cf0342&is=65bc8e42&hm=d538e6fa983d1657994468e934740c932916c043b5c4cb692f2d79d514731eb2&=&format=webp&quality=lossless&width=502&height=502') //You can put links in tenor or discord and etc.
+    .setAssetsLargeText('Nitro & Exitlag available !') //Text when you hover the Large image
+    .setAssetsSmallImage('https://cdn.discordapp.com/emojis/1117852451236761691.gif?size=96&quality=lossless') //You can put links in tenor or discord and etc.
+    .setAssetsSmallText('Legit Discord Shop !') //Text when you hover the Small image
+    .addButton('⇢˗ˏˋShop࿐ྂ', 'https://discord.com/invite/eporium')
+    .addButton('⇢˗ˏˋVouches࿐ྂ', 'https://discord.gg/bG6PgpBA2P');
 
   client.user.setActivity(r);
   client.user.setPresence({ status: "dnd" }); //dnd, online, idle, offline
@@ -44,7 +44,7 @@ client.on('ready', async () => {
   setInterval(() => {
     const newTime = formatTime();
     if (newTime !== prevTime) {
-      const newDetails = ` [${newTime}]`;
+      const newDetails = `.gg/eporium`;
       r.setDetails(newDetails);
       client.user.setActivity(r);
       prevTime = newTime;
